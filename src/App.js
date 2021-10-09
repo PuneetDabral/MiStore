4cfa76599aef6405329b2818e0723f169b6a4268
PuneetDabral <86554356+PuneetDabral@users.noreply.github.com>
2021-10-10 00:31:58 +0530

    add slider for home page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 
 import './App.css';
-import Navbar from './components/Navbar';
-import PrenavBar from './components/PrenavBar';
+import Navbar from './components/Navbar.js';
+import PrenavBar from './components/PrenavBar.js';
+import {  BrowserRouter as Router} from "react-router-dom"
+import Slider from './components/Slider'
+import {banner} from './data/data.json'
 
 function App() {
   return (
-    <div >
+    <Router>
     <PrenavBar />
     <Navbar />
+    <Slider start={banner.start} />
   
-    </div>
+    </Router>
   );
 }
 
