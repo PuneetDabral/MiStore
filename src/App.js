a8266e61e4a029d16945a0fbbcf9d805b493ce2c
PuneetDabral <86554356+PuneetDabral@users.noreply.github.com>
2021-10-15 23:59:14 +0530

    complete all website

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@
 import './App.css';
 import Navbar from './components/Navbar.js';
 import PrenavBar from './components/PrenavBar.js';
-import {  BrowserRouter as Router} from "react-router-dom"
+import {  BrowserRouter as Router,Route } from "react-router-dom"
 import Slider from './components/Slider'
 import data from './data/data.json'
 import Offers from './components/Offers.js'
 import Heading  from "./components/Heading.js"
 import StarProduct from "./components/StarProduct.js"
+import HotAccessoriesMenu  from "./components/HotAccessoriesMenu.js"
+import HotAccessories from "./components/HotAccessories.js"
+import ProductReviews from "./components/ProductReviews.js"
+import Videos from "./components/Videos.js"
+import Banner from "./components/Banner.js"
+import Footer from "./components/Footer.js"
+// import NavOptios from "./components/NavOptios.js"
 
 
 
@@ -20,6 +27,42 @@ function App() {
     <Offers offer={data.offer}/>
     <Heading text="STAR PRODUCTS"/>
     <StarProduct starProduct={data.starProduct} />
+    <Heading text="HOT ACESSORIES"/>
+    <HotAccessoriesMenu />
+    <Route exact path="/music">
+       <HotAccessories music={data.hotAccessories.music} musicCover={data.hotAccessoriesCover.music}  />
+       </Route>
+
+       <Route exact path="/smartDevice">
+       <HotAccessories smartDevice={data.hotAccessories.smartDevice} smartDeviceCover={data.hotAccessoriesCover.smartDevice}  />
+       </Route>
+
+       <Route exact path="/home">
+       <HotAccessories home={data.hotAccessories.home} homeCover={data.hotAccessoriesCover.home}  />
+       </Route>
+
+       <Route exact path="/lifestyle">
+       <HotAccessories lifeStyle={data.hotAccessories.lifeStyle} lifeStyleCover={data.hotAccessoriesCover.lifeStyle}  />
+       </Route>
+
+
+       <Route exact path="/mobileAccessories">
+       <HotAccessories mobileAccessories={data.hotAccessories.mobileAccessories} mobileAccessoriesCover={data.hotAccessoriesCover.mobileAccessories}  />
+       </Route>
+
+       <Heading text="PRODUCT REVIEWS"/>
+
+       <ProductReviews productReviews={data.productReviews}/>
+
+       <Heading text="VIDEOS" />
+      
+      <Videos videos={data.videos} />
+
+      <Heading  text="IN THE PRESS"/>
+
+      <Banner  banner={data.banner}/>
+
+       <Footer footer={data.footer} />
   
     </Router>
   );
